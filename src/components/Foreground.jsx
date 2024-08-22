f8b732d2198da7148cb6fd2b0cac57154e564654
Jaipal singh sisodiya <jaipalsinghsisodiya46@gmail.com>
2024-08-22 22:27:00 +0530

    Mini DocsApp draging enabled

--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,10 +1,8 @@
 import React, { useRef } from "react";
 import Card from "./Card";
 
-
 function Foreground() {
-
-const ref = useRef(null);
+  const ref = useRef(null);
 
   const data = [
     {
@@ -28,7 +26,10 @@ const ref = useRef(null);
   ];
   return (
     <>
-      <div ref={ref} className="fixed top-0 left-0 z-[3] w-full h-full flex gap-10 flex-wrap p-5">
+      <div
+        ref={ref}
+        className="fixed top-0 left-0 z-[3] w-full h-full flex gap-10 flex-wrap p-5"
+      >
         {data.map((item, index) => (
           <Card data={item} reference={ref} />
         ))}
